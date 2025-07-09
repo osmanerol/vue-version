4a3218d8d6bebe3f93ebe73e9899ba40673b9b85
osmanerol <osmanerol98@outlook.com>
2025-07-09 17:42:59 +0300

    .

--- update-version.js
+++ update-version.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const versionFile = path.join(__dirname, 'version.json')
+const versionFile = path.join(__dirname, '/public/version.json')
 
 const incrementVersion = version => {
   const parts = version.split('.')
