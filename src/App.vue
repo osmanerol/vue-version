6ca2bcabb5163fc8d277ca61acaf280f62e26895
osmanerol <osmanerol98@outlook.com>
2025-07-09 17:40:25 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const versionInterval = ref(null)
+
+onMounted(() => {
+  checkVersion()
+  startVersionInterval()
+})
+
+const startVersionInterval = () => {
+  versionInterval.value = setInterval(() => {
+    checkVersion()
+  }, 10 * 1000)
+}
+
+const checkVersion = async () => {
+  const currentVersion = localStorage.getItem('version')
+  const response = await fetch('/version.json', { cache: 'no-store' })
+  const data = await response.json()
+  if(!currentVersion) {
+    localStorage.setItem('version', data?.version)
+  }
+  else if(currentVersion != data.version) {
+    localStorage.setItem('version', data?.version)
+    clearInterval(versionInterval.value)
+    location.reload(true)
+  }
+}
+</script>
+
+<template>
+  <h1>Vue Version</h1>
+</template>
+
+<style scoped>
+</style>
---
+++ update-version.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const versionFile = path.join(__dirname, 'version.json')
+
+const incrementVersion = version => {
+  const parts = version.split('.')
+  const patch = parseInt(parts[2], 10) + 1
+  return `${parts[0]}.${parts[1]}.${patch}`
+}
+
+const data = fs.readFileSync(versionFile, 'utf8')
+const json = JSON.parse(data)
+const oldVersion = json.version
+const newVersion = incrementVersion(oldVersion)
+json.version = newVersion
+fs.writeFileSync(versionFile, JSON.stringify(json, null, 2))
